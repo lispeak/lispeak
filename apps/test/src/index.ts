a9112842108d2aacb63ee00fe706ad8bfb45017d
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-03-03 18:45:23 +0400

    Fix

--- apps/test/src/index.ts
+++ apps/test/src/index.ts
@@ -11,14 +11,3 @@ console.log({ booleanFalse });
 console.log({ booleanOperationNot });
 console.log({ booleanOperationAnd });
 console.log({ booleanOperationOr });
-
-const createGreeting = (subject: string) => `test service live for ${subject}`;
-
-export const start = () => {
-  const subject = process.env.TEST_SUBJECT ?? 'developers';
-  console.log(createGreeting(subject));
-};
-
-if (import.meta.main) {
-  start();
-}
